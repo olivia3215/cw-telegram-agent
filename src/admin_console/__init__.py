b9fd48f6e0dda259cddff95ad1d84e979f79f42e
olivia3215 <olivia3215@cindysworld.org>
2025-11-11 09:59:17 -0800

    Deduplicate admin_console exports

--- src/admin_console/__init__.py
+++ src/admin_console/__init__.py
@@ -3,7 +3,3 @@
 from .app import create_admin_app, start_admin_console  # noqa: F401
 
 __all__ = ["create_admin_app", "start_admin_console"]
-from .app import create_admin_app, start_admin_console
-
-__all__ = ["create_admin_app", "start_admin_console"]
-
